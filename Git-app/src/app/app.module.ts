cc6c76dcc84392fd470b4d589fbc2caa1eb6f9c8
Samuel-dot-cloud <successam336@gmail.com>
2020-04-09 13:20:26 +0300

    creation of folders for arrangement of various components, classes, services and directives

--- Git-app/src/app/app.module.ts
+++ Git-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchBarComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
